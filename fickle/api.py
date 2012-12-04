0305f7a82f72c24e5b3a27ef09d82dfe3b2b3482
Norbert Crombach <norbert.crombach@primetheory.org>
2012-12-04 18:39:17 +0100

    Refactor fickle package

---
+++ fickle/api.py
@@ -0,0 +1,48 @@
+import flask
+from flask import request, json
+
+def Response(data, status = 200):
+    body = json.dumps(data)
+    return flask.Response(body, status = status, mimetype = 'application/json')
+
+def SuccessResponse(dataset_id = None):
+    return Response({ 'success': True, 'id': dataset_id })
+
+def ErrorResponse(status = 400):
+    return Response({ 'success': False }, status = status)
+
+def API(name, backend):
+    app = flask.Flask(name)
+    app.config.from_object(name)
+
+    @app.route('/')
+    def api_root():
+        return SuccessResponse(backend.dataset_id)
+
+    @app.route('/load', methods=['POST'])
+    def api_load():
+        backend.load(request.json)
+        return SuccessResponse(backend.dataset_id)
+
+    @app.route('/fit', methods=['POST'])
+    def api_fit():
+        if not backend.loaded():
+            return ErrorResponse()
+        backend.fit()
+        return SuccessResponse(backend.dataset_id)
+
+    @app.route('/validate', methods=['POST'])
+    def api_validate():
+        if not backend.loaded():
+            return ErrorResponse()
+        data = backend.validate()
+        return Response(data)
+
+    @app.route('/predict', methods=['PUT'])
+    def api_predict():
+        if not backend.trained():
+            return ErrorResponse()
+        data = backend.predict(request.json).tolist()
+        return Response(data)
+
+    return app
---
+++ fickle/backend.py
@@ -0,0 +1,42 @@
+import sklearn.cross_validation
+
+class Backend(object):
+    def __init__(self):
+        self.dataset_id = 0
+        self.dataset = None
+        self.model = None
+
+    def load(self, dataset):
+        self.model = None
+        self.dataset_id += 1
+        self.dataset = dataset
+        self._data = dataset['data']
+        self._target = dataset['target']
+        return True
+
+    def loaded(self):
+        return (self.dataset is not None)
+
+    def fit(self):
+        if not self.loaded():
+            return False
+        model = self.classifier()
+        model.fit(self._data, self._target)
+        self.model = model
+        return True
+
+    def trained(self):
+        return (self.model is not None)
+
+    def validate(self, test_size = 0.2, random_state = 0):
+        if not self.loaded():
+            return
+        model = self.classifier()
+        X_train, X_test, y_train, y_test = sklearn.cross_validation.train_test_split(
+            self._data, self._target, test_size = test_size, random_state = random_state
+        )
+        model.fit(X_train, y_train)
+        return [model.score(X_test, y_test)]
+
+    def predict(self, value):
+        return self.model.predict(value)
