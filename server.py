86f9fb93725da3ccc308d1957a64e932835f1823
Norbert Crombach <norbert.crombach@primetheory.org>
2012-12-28 17:25:56 +0100

    Copy run section from Heroku example

--- server.py
+++ server.py
@@ -4,4 +4,9 @@ from fickle.classifier import GenericSVMClassifier
 backend = GenericSVMClassifier()
 
 app = API(__name__, backend)
-app.run(debug = True)
+
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = int(os.environ.get('PORT', 5000))
+    debug = bool(os.environ.get('FICKLE_DEBUG'))
+    app.run(host = host, port = port, debug = debug)
