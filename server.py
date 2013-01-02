70167a8cb73673e1e904fbeb8a50b3de9d4fc1ae
Norbert Crombach <norbert.crombach@primetheory.org>
2013-01-02 18:17:49 +0100

    Fix run section with import statement

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ backend = GenericSVMClassifier()
 app = API(__name__, backend)
 
 if __name__ == '__main__':
+    import os
     host = '0.0.0.0'
     port = int(os.environ.get('PORT', 5000))
     debug = bool(os.environ.get('FICKLE_DEBUG'))
