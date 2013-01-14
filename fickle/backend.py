42768d1c05ac78e4ea42273907ee425d4f2445ee
Norbert Crombach <norbert.crombach@primetheory.org>
2013-01-14 18:48:20 +0100

    Support reloading with cPickle and pylibmc

--- fickle/backend.py
+++ fickle/backend.py
@@ -1,4 +1,16 @@
+import os
+import pickle
+import pylibmc
+
 import sklearn.cross_validation
+
+CACHE_KEY = 'fickle:predictor'
+CACHE = pylibmc.Client(
+        servers = [os.environ.get('MEMCACHE_SERVERS', '127.0.0.1')],
+        username=os.environ.get('MEMCACHE_USERNAME'),
+        password=os.environ.get('MEMCACHE_PASSWORD'),
+        binary=True
+)
 
 class Backend(object):
     def __init__(self):
@@ -16,21 +28,22 @@ class Backend(object):
         return True
 
     def loaded(self):
-        return (self.__dataset is not None)
+        return bool(self.__dataset)
 
     def fit(self):
         if not self.loaded():
-            return False
+            return
         model = self.model()
         model.fit(self.__data, self.__target)
         self.__model = model
+        self.__dump()
         return True
 
     def trained(self):
-        return (self.__model is not None)
+        return bool(self.__model)
 
     def predict(self, value):
-        if not self.trained():
+        if not self.trained() and not self.__load():
             return
         return self.__model.predict(value)
 
@@ -46,3 +59,18 @@ class Backend(object):
         )
         model.fit(X_train, y_train)
         return [model.score(X_test, y_test)]
+
+    def __load(self):
+        string = CACHE.get(CACHE_KEY)
+        if not string:
+            return
+        model = pickle.loads(string)
+        self.__model = model
+        return True
+
+    def __dump(self):
+        if not self.trained():
+            return
+        string = pickle.dumps(self.__model)
+        CACHE.set(CACHE_KEY, string)
+        return True
